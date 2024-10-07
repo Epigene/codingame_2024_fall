ec222f680c8b45509bbf6dc270c198d33bf2ca59
Epigene <augusts.bautra@gmail.com>
2024-10-07 21:58:59 +0300

    2024-10-07 define cluser example 4

--- lib/requires.rb
+++ lib/requires.rb
@@ -7,3 +7,10 @@ STDOUT.sync = true # DO NOT REMOVE
 def debug(message, log_level = 2)
   STDERR.puts(message) if log_level <= 2
 end
+
+# @param time [Float] seconds strsight from Benchmark.relatime
+def report_time(time, message)
+  return if time < 0.01 # as in 10ms
+
+  debug("Took #{(time * 1000).round}ms to #{message}", 0)
+end
